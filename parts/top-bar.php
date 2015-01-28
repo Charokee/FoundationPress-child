5440bab14c25d64b050ad7649ecb7c452f7a2266
Charokee <simon.aumayer@aboutlabs.de>
2015-01-28 12:54:27 +0100

    added file includes for responsive navigation

---
+++ parts/top-bar.php
@@ -0,0 +1,14 @@
+<div class="show-for-medium-up" role="navigation">
+    <nav class="top-bar" data-topbar="">
+        <section class="top-bar-section">
+            <?php foundationPress_top_bar_l(); ?>
+            <?php foundationPress_top_bar_r(); ?>
+        </section>
+        <a href="https://www.facebook.com/Sinnlicht.Yogazentrum" target="_blank">
+            <img src="/wp-content/themes/sinnlicht-yoga/assets/img/social/facebook.png" class="right header__socialmedia" width="30" height="30" alt="Facebook" />
+        </a>
+        <a href="http://www.yogatrail.com/studio/sinnlicht-yoga-75227" target="_blank">
+            <img src="/wp-content/themes/sinnlicht-yoga/assets/img/social/yogatrail.png" class="right header__socialmedia" width="30" height="30" alt="Yogatrail" />
+        </a>
+    </nav>
+</div>
