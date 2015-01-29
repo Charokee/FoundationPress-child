3aac532a4a92962a9c321520d2cb0e5aeb301115
Charokee <simon.aumayer@aboutlabs.de>
2015-01-29 12:24:59 +0100

    set minimal height for detail page

--- page.php
+++ page.php
@@ -65,20 +65,18 @@
             </div>          
         </header>
             
-<section class="container main-content-area__section--offset" role="document">
+<section class="container main-content-area__section--offset main-content-area__section-wrapper-background" role="document">
 	<?php do_action('foundationPress_after_header'); ?>
 
-<div class="row main-content-area__section-background main-content-area__section--full-height">
-	<div class="small-12 columns" role="main">
+<div class="row">
+	<div class="small-12 columns main-content-area__section-background detail-page--full-height" role="main">
 
 	<?php do_action('foundationPress_before_content'); ?>
 
 	<?php while (have_posts()) : the_post(); ?>
-            <header>
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-            </header>
             <?php do_action('foundationPress_page_before_entry_content'); ?>
             <div class="entry-content">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
                     <?php the_content(); ?>
             </div>
 	<?php endwhile;?>
@@ -87,4 +85,4 @@
 
 	</div>
 </div>
-<?php get_footer(); ?>
+<?php get_footer('detailpage'); ?>
