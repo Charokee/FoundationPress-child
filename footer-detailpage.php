25e7bb2725f427a554c63f7c879aba6a56e0640a
Charokee <simon.aumayer@aboutlabs.de>
2015-01-29 13:12:44 +0100

    added specialized footer for detail page

---
+++ footer-detailpage.php
@@ -0,0 +1,31 @@
+</section>
+<footer class="row main-content-area__section-background">
+    <?php do_action('foundationPress_before_footer'); ?>
+    <div class="small-12 columns">
+        <ul class="inline-list">
+            <li>
+                <a href="<?php echo home_url() . '/raumvermietung'; ?>">Raumvermietung</a>
+            </li>
+            <li>
+                <a href="<?php echo home_url() . '/freunde'; ?>">Freunde</a>
+            </li>
+            <li>
+                <a href="<?php echo home_url() . '/impressum'; ?>">Impressum</a>
+            </li>
+            <li>
+                <a href="<?php echo home_url() . '/agb'; ?>">AGB</a>
+            </li>
+        </ul>
+    </div>
+    <?php dynamic_sidebar("footer-widgets"); ?>
+        <?php do_action('foundationPress_after_footer'); ?>
+</footer>
+<a class="exit-off-canvas"></a>
+
+	<?php do_action('foundationPress_layout_end'); ?>
+	</div>
+</div>
+<?php wp_footer(); ?>
+<?php do_action('foundationPress_before_closing_body'); ?>
+</body>
+</html>
