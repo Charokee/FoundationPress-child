71e13b5e7f23b8905827a65f8b01735d49fbfb87
Charokee <simon.aumayer@aboutlabs.de>
2015-01-29 16:35:53 +0100

    adapted scrolling offset for proper scrolling behavior

--- js/helper-scripts.js
+++ js/helper-scripts.js
@@ -29,7 +29,7 @@ $(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
         var target = $(this.hash);
-        var headerOffset = 181;
+        var headerOffset = 60;
 
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
