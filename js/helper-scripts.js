e9e85e86e2f3615729a7555e17c6b21d047092f8
Charokee <simon.aumayer@aboutlabs.de>
2015-01-31 14:36:00 +0100

    refactored and adapted jquery for responsive typography and better load handling

--- js/helper-scripts.js
+++ js/helper-scripts.js
@@ -1,6 +1,6 @@
-// Hiding or showing the logo dynamically
 $(document).ready(function ($) {
 
+    // Hiding or showing the logo dynamically
     $(window).scroll(function () {
         if ($(window).innerWidth() >= 784) {
             var scrollTop = $(window).scrollTop();
@@ -10,58 +10,59 @@ $(document).ready(function ($) {
                 $('#logo').removeClass('header__logo--show');
                 $('#logo').addClass('header__logo--hide');
                 $('header').addClass('fixed');
-                $('.container').addClass('main-content-area__section--offset-medium');
+                $('.container').addClass('main-content-area__section--offset');
             } else {
                 $('.header__logo').removeClass('header__logo--hide');
                 $('.header__logo').addClass('header__logo--show');
                 $('#logo').removeClass('header__logo--hide');
                 $('#logo').addClass('header__logo--show');
                 $('header').removeClass('fixed');
-                $('.container').removeClass('main-content-area__section--offset-medium');
+                $('.container').removeClass('main-content-area__section--offset');
             }
         } else {
             $('header').addClass('fixed');
-            $('.container').addClass('main-content-area__section--offset-small');
+            $('.container').addClass('main-content-area__section--offset');
         }
     });
 
-    $('ul#menu-hauptmenue li a').click(function () {
+    // collapsing the mobile menu on menu item click
+    $('ul#menu-hauptmenue-medium li a').click(function () {
         $('nav.top-bar').removeClass('expanded');
     });
 
-});
+    // Scrolling to anchors in document with Offset
+    $(function () {
 
-// Scrolling to anchors in document with Offset
-$(function () {
+        if ($(window).innerWidth() <= 784) {
+            headerOffset = 40;
+        } else {
+            headerOffset = 60;
+        }
 
-    if ($(window).innerWidth() <= 784) {
-        headerOffset = 40;
-    } else {
-        headerOffset = 60;
-    }
+        $('a[href*=#]:not([href=#])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - headerOffset // offsets for fixed header
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
 
-    $('a[href*=#]:not([href=#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        //Executed on page load with URL containing an anchor tag.
+        if ($(location.href.split('#')[1])) {
+            var target = $('#' + location.href.split('#')[1]);
             if (target.length) {
                 $('html,body').animate({
-                    scrollTop: target.offset().top - headerOffset // offsets for fixed header
+                    scrollTop: target.offset().top - headerOffset // offset height of header here too.
                 }, 1000);
                 return false;
             }
         }
-    });
 
-    //Executed on page load with URL containing an anchor tag.
-    if ($(location.href.split('#')[1])) {
-        var target = $('#' + location.href.split('#')[1]);
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top - headerOffset // offset height of header here too.
-            }, 1000);
-            return false;
-        }
-    }
+    });
 
 });
