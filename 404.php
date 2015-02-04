9da220845132fc7fa1516f2332697be4254fd22c
Charokee <simon.aumayer@aboutlabs.de>
2015-02-04 11:21:48 +0100

    modified 404 page: redirects now on home url

--- 404.php
+++ 404.php
@@ -1,23 +1,5 @@
-<?php get_header(); ?>
-
-<div class="main-content-area__section-wrapper-background">
-    <div class="row">
-        <div class="small-12 columns main-content-area__section-background detail-page--full-height" role="main">
-            <div class="entry-content">
-                <header>
-                    <h1 class="entry-title"><?php _e('File Not Found', 'FoundationPress'); ?></h1>
-                </header>
-                <div class="error">
-                    <p class="bottom"><?php _e('The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.', 'FoundationPress'); ?></p>
-                </div>
-                <p><?php _e('Please try the following:', 'FoundationPress'); ?></p>
-                <ul>
-                    <li><?php _e('Check your spelling', 'FoundationPress'); ?></li>
-                    <li><?php printf(__('Return to the <a href="%s">home page</a>', 'FoundationPress'), home_url()); ?></li>
-                    <li><?php _e('Click the <a href="javascript:history.back()">Back</a> button', 'FoundationPress'); ?></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-<?php get_footer('detailpage'); ?>
+<?php
+    header("HTTP/1.1 301 Moved Permanently");
+    header("Location: " . get_bloginfo('url'));
+    exit();
+?>
